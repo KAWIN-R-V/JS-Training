e69c3d3503c8cad21916bd4709a0987b756b1730
KAWIN-R-V <kawin05rv@gmail.com>
2026-07-07 12:11:25 +0530

    nodejs/session1: complete activity tasks

--- nodejs/session1/compare.js
+++ nodejs/session1/compare.js
@@ -1,6 +1,6 @@
 // This works in Node.js
-console.log("Node version:", process.version)
-console.log("Platform:", process.platform)
+console.log("Node version: ", process.version)
+console.log("Platform: ", process.platform)
 
 // Try adding this and run the file — observe the error
 // console.log(document.querySelector("h1"))
